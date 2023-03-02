8cbd416c2ae17f24e99907428fd5bc7cdaf80490
Laurent Michel <laurent.michel@cognex.com>
2023-03-02 09:27:39 +0100

    fix terraform config

--- vnets/outputs.tf
+++ vnets/outputs.tf
@@ -2,7 +2,7 @@ output "aks_snet_id" {
   value = element([
     for subnet in azurerm_virtual_network.vnet_cluster.subnet :
     subnet.id
-    if subnet.name == local.aks_subnet_name
+    if subnet.name == "AksSubnet"
   ], 0)
 }
 
@@ -10,7 +10,7 @@ output "resources_snet_id" {
   value = element([
     for subnet in azurerm_virtual_network.vnet_hub.subnet :
     subnet.id
-    if subnet.name == local.resources_subnet_name
+    if subnet.name == "ResourcesSubnet"
   ], 0)
 }
 
